346e996b2f51153055c39e9b37938a22f8452139
junaidhkn <junaidhkniazi@gmail.com>
2023-07-26 14:17:10 +0500

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
+import Link from 'next/link'
+
 export default function Home() {
-	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-			<h1>Home page</h1>
-		</main>
-	);
+  return (
+    <main className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-5xl mb-4">
+          EmoTrack: Capturing Your Life&apos;s Emotional Journey with AI
+        </h1>
+        <p className="text-2xl text-white/60 mb-4">
+          The cutting-edge AI-powered web app designed to track and understand
+          your emotions throughout life&apos;s incredible journey.
+        </p>
+        <p className="text-[16px] mb-6">
+          Seamlessly connect with your inner self as you share your day&apos;s
+          experiences through a simple paragraph. Our advanced AI algorithms
+          will decipher your emotions, providing valuable insights and helping
+          you gain a deeper understanding of your emotional well-being. Embrace
+          the power of EmoTrack and embark on a transformative emotional
+          exploration like never before.
+        </p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-lg">
+              Get started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </main>
+  )
 }
