d8c07edaad433c2024f54a2882ad1f028fd37d62
junaidhkn <junaidhkniazi@gmail.com>
2023-07-28 16:36:37 +0500

    get journal entries

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,22 +1,23 @@
 import { getUserFromClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 
-const JournalPage = async () => {
+const getEntries = async () => {
   const user = await getUserFromClerkID()
-  const entries = await prisma.user.findMany({
+  const entries = await prisma.journalEntry.findMany({
     where: {
       userId: user.id as string,
     },
     orderBy: {
       createdAt: 'desc',
     },
-  }
+  })
   return entries
-  
-  )
 }
-return <div>JournalPage</div>
 
+const JournalPage = async () => {
+  const entries = await getEntries()
+  console.log(entries)
+  return <div>JournalPage</div>
 }
 
 export default JournalPage
